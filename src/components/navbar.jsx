8cf375e04a9f548cee979696e9e38bc131dc87f3
Fathir achmad sabiilah <Fathir17.fa@gmail.com>
2023-06-08 19:28:28 +0700

    First commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div>
+            <div class="main">
+                <div class="navbar">
+                    <div class="nav"><a href="/">HOME</a></div>
+                    <div class="nav"><a href="./about">ABOUT</a></div>
+                    <div class="nav"><a href="./skill">SKILL</a></div>
+                    <div class="nav"><a href="./contact">CONTACT</a></div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/skill.jsx
@@ -0,0 +1,46 @@
+import "../style/style.css";
+
+export default function Skill() {
+    return(
+        <div>
+            <h1 class="skill-text">SKILL</h1>
+    <div class="skill">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus natus rem reprehenderit rerum iste. Ex
+        mollitia architecto adipisci quis deserunt ullam, amet aut perspiciatis sunt praesentium, dolorum nam neque quo
+        soluta quam cupiditate fugit. Esse fugit molestias aperiam quidem illum quisquam dolores, voluptates nisi
+        officiis. Reiciendis nesciunt et quia illum saepe ullam beatae. Sit ea hic est expedita a nostrum aut
+        consectetur cum vero molestias neque perferendis libero quae, nihil atque corrupti perspiciatis recusandae
+        repellendus veniam veritatis aspernatur consequatur temporibus iste. Accusamus, qui vel consequuntur enim sequi
+        autem quis fuga facere rem odio unde! Ipsum tempora dolores ratione quae obcaecati mollitia minus nemo ipsa
+        fugiat corporis, sapiente praesentium odio neque sed, eligendi consectetur illum itaque, temporibus ex numquam!
+        A quo esse aspernatur quasi inventore deleniti praesentium similique id non illo ab magni error, eius tempora,
+        tenetur numquam odio debitis repudiandae! Enim modi laboriosam eaque unde. Eveniet ducimus rerum eos delectus
+        ullam a provident itaque aliquam, error reprehenderit magni porro reiciendis dolore, ab facilis tenetur
+        repellat, corporis laborum! Atque placeat molestiae delectus temporibus! Sint pariatur veniam laudantium velit
+        enim, assumenda distinctio magni qui ullam, dolorum praesentium! Praesentium inventore excepturi corporis
+        officiis illum, dolorum id cum dicta beatae blanditiis et culpa, itaque sunt doloremque eaque voluptatum unde!
+        Quae quam consequuntur deserunt exercitationem ipsa velit eligendi reiciendis. Nulla tempore ipsa ea voluptatem
+        consequatur quos sed id, dolorum quis sint quisquam sapiente fuga. Aspernatur provident laudantium nostrum atque
+        facilis sunt consequatur eum obcaecati non vero velit eius tempora optio, temporibus, praesentium dolorum.
+        Assumenda repellendus consequatur rerum alias quis dolores quos, delectus minima officiis fugit accusantium eum
+        temporibus ipsa harum ex sequi aperiam cupiditate et? Optio excepturi incidunt alias? Eius, harum! Perferendis
+        fugiat, fugit quae nemo culpa voluptatibus. Reiciendis autem minima ut quibusdam vero porro iure iste! Omnis,
+        consequatur error ipsum atque facilis mollitia libero hic deserunt cupiditate quos asperiores sequi, repudiandae
+        accusamus, laboriosam explicabo? Deserunt, temporibus. Facere reiciendis repellat ut suscipit vel nesciunt
+        architecto inventore eligendi mollitia voluptatibus facilis quae minus maiores quos sunt a modi ad nulla
+        recusandae sed dicta, ab doloremque. Quidem iste nesciunt dolorum, similique ducimus itaque explicabo nulla
+        sequi eos pariatur nostrum. Accusamus ipsa quam minus dolorum ducimus blanditiis omnis ea laudantium minima
+        molestias odit libero inventore, voluptatibus laborum repellat voluptatem harum distinctio quaerat consequuntur
+        iste corporis necessitatibus, natus porro at. Itaque soluta, unde omnis repudiandae eius saepe temporibus velit.
+        Veniam, at deserunt. Suscipit quis accusantium numquam inventore ex odio est quia, dolorem quae ratione tempora
+        molestiae similique esse debitis fuga eius expedita voluptatibus facere in, nulla quos consequatur eos totam
+        aut! Ipsa numquam ad, incidunt nobis exercitationem, repudiandae magni quam necessitatibus beatae aspernatur
+        possimus! Laudantium numquam fugit fugiat quod ipsam at reprehenderit ipsa omnis doloremque, illo porro dolores
+        sequi perferendis earum dicta. Praesentium dolorum quis consectetur optio? Accusamus aut natus omnis. Nobis
+        distinctio quos labore exercitationem odit minus facere accusantium consequuntur quasi corporis. Atque molestias
+        culpa libero autem voluptatem veritatis nobis vero exercitationem suscipit ut aliquid repudiandae recusandae,
+        excepturi maxime at placeat quibusdam perferendis voluptas in mollitia fugit cupiditate.
+    </div>
+        </div>
+    )
+}
